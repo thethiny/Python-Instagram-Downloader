ce8013d5c20e07b61264078509c0aa063975261c
thethiny <thethiny@ymail.com>
2022-10-20 02:29:53 +0400

    Support Tagged Posts and handle unknown users

--- src/api.py
+++ src/api.py
@@ -1,13 +1,14 @@
 import json
 import os
-from typing import Iterable, List
+import shutil
+from typing import Dict, Iterable, List, Optional
 
 import requests
 
 from src.consts import (FEED_API, IG_HEADERS, PROFILE_INFO_GRAPH_API,
                         STORY_API, USER_ID_API)
-from src.utils import download_item, get_extension_from_url
-from src.validators import ParsedItemType, ReelItemType, UserType
+from src.utils import download_item, get_extension_from_url, set_creation_time
+from src.validators import ParsedItemType, ParsedTagUserType, ReelItemType, UserMediaTagType, UserType
 
 
 class InstagramDownloader:
@@ -129,6 +130,8 @@ class InstagramDownloader:
     def parse_reel_item(self, item: ReelItemType) -> ParsedItemType:
         item_id = item["pk"]
         owner = item["user"]["pk"]
+        owner_user = item["user"].get("username", "")
+        timestamp = item.get("taken_at", 0)
         parent_id = item.get("carousel_parent_id") or item.get("parent_id")
         close_friends_only = item.get("audience", "") == "besties"
         has_images = "image_versions2" in item
@@ -139,13 +142,26 @@ class InstagramDownloader:
             biggest_video = max(item["video_versions"], key=lambda x: x["width"] * x["height"])["url"]
         if not has_images:
             raise Exception(f"Failed to find image {item_id} for reel")
+        usertags: Dict[str, List[UserMediaTagType]] = item.get("usertags", {})
+
+        tags: List[ParsedTagUserType] = [
+            {
+                "id": obj["user"]["pk"],
+                "username": obj["user"]["username"]
+            }
+            for tag_list in usertags.values()
+            for obj in tag_list
+        ]
         return {
             "id": item_id,
             "owner": owner,
+            "owner_username": owner_user,
+            "tagged_users": tags,
             "image_url": biggest_photo,
             "video_url": biggest_video,
             "besties_only": close_friends_only,
             "parent": parent_id,
+            "time": timestamp,
         }
 
     def parse_post_item(self, item) -> List[ParsedItemType]:    
@@ -155,49 +171,98 @@ class InstagramDownloader:
         post_items: List[ParsedItemType] = []
         for carousel_item in item["carousel_media"]:
             carousel_item["user"] = item["user"]
+            carousel_item["taken_at"] = item["taken_at"]
             post_item = self.parse_reel_item(carousel_item)
             post_items.append(post_item)
         
         return post_items
 
     def download_list(self, downloads_list: List[ParsedItemType], mappings, folder, download_path):
-        all_exist = True
+        default_path = os.path.join(download_path, "{owner}", folder)
         for i, item in enumerate(downloads_list):
             parent_id = item["parent"]
             id_ = item["id"]
-            if parent_id:
-                image_name = video_name = f"{parent_id}_{id_}"
-            else:
-                image_name = video_name = id_
+            image_name = video_name = f"{parent_id}_{id_}" if parent_id else id_
+
             try:
                 owner = mappings[str(item["owner"])]
             except KeyError:
-                print("Uknown Owner", item["owner"])
-                owner = os.path.join("Unknown", str(item["owner"]))
+                print("Possible Repost found", item["owner"])
+                # owner = os.path.join("Unknown", str(item["owner"]))
+                owner = item["owner_username"] or "Unknown"
             print(f"Downloading item {id_} for {owner} ({i+1}/{len(downloads_list)})", end="\r")
+
             image = item["image_url"]
             video = item["video_url"]
             besties = item["besties_only"]
+            time = item["time"]
+
             image_ext = get_extension_from_url(image)
             video_ext = get_extension_from_url(video)
-            path = os.path.join(download_path, owner, folder)
-            os.makedirs(path, exist_ok=True)
-            if besties:
-                path = os.path.join(path, "private")
 
-            thumbnails_path = os.path.join(path, "video_thumbnails")
-            os.makedirs(thumbnails_path,exist_ok=True)
+            image_path, video_path = self._get_media_out_paths(default_path, besties, video, owner, False)
+            os.makedirs(image_path, exist_ok=True)
 
             if video:
-                image_name = os.path.join("video_thumbnails", image_name + "_thumbnail")
-                video_file = os.path.join(path, f"{video_name}.{video_ext}")
-                if download_item(video, video_file):
-                    all_exist = False
-            
-            image_file = os.path.join(path, f"{image_name}.{image_ext}")
-            if download_item(image, image_file):
-                all_exist = False
+                os.makedirs(video_path, exist_ok=True)
+                image_name = image_name + "_thumbnail"
+                video_file = os.path.join(video_path, f"{video_name}.{video_ext}")
+                download_item(video, video_file, time)
+
+            image_file = os.path.join(image_path, f"{image_name}.{image_ext}")
+            download_item(image, image_file, time)
+
+            tagged_users = item["tagged_users"]
+            if tagged_users:
+                print("Handling Tags in item")
+                for user_obj in tagged_users:
+                    if not self._is_user_tracked(user_obj["id"], user_obj["username"], mappings, download_path):
+                        continue
+                    tag_user = user_obj["username"] or mappings.get(str(user_obj["id"]))
+                    im_copy, vd_copy = self._get_media_out_paths(default_path, besties, video, tag_user, True)
+                    os.makedirs(im_copy, exist_ok=True)
+                    self._copy_item(image_file, os.path.join(im_copy, f"{image_name}.{image_ext}"), time)
+                    if video:
+                        os.makedirs(vd_copy, exist_ok=True)
+                        self._copy_item(video_file, os.path.join(vd_copy, f"{video_name}.{video_ext}"), time) # type: ignore
+                    
         print()
-        return all_exist
+
+    def _copy_item(self, from_, to_, time):
+        if os.path.exists(to_):
+            return False
+        shutil.copy2(from_, to_) # type: ignore
+        if time > 0:
+            set_creation_time(to_, time)
+        return True
+
+    def _is_user_tracked(self, user_id: str, user_name: str, mappings: dict, media_path: str):
+        if str(user_id) in mappings:
+            return True
+
+        if not user_name or user_name == "Unknown":
+            return False
+        
+        if os.path.exists(os.path.join(media_path, user_name, "meta")): # Needs to have meta
+            return True
+        return False
+        
+
+    def _get_media_out_paths(self, default_path: str, is_private: bool, has_video: Optional[str], owner: str, is_tag: bool):
+        path = default_path
+        if is_tag:
+            path = os.path.join(path, "tagged")
+        if is_private:
+            path = os.path.join(path, "private")
+        if has_video:
+            image_path = os.path.join(path, "video_thumbnails")
+            video_path = path
+        else:
+            image_path = path
+            video_path = ""
+
+        return [s.format(owner=owner) for s in [image_path, video_path]]
+
+
 
             
