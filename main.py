fa236433959cd15d8abc05f0db6358209b09e231
thethiny <thethiny@ymail.com>
2022-10-20 02:29:37 +0400

    Add All Users flag and change list storage

--- main.py
+++ main.py
@@ -6,43 +6,138 @@ from typing import Dict, List
 
 from src.api import InstagramDownloader
 from src.consts import LIMIT, MEDIA_PATH
-from src.utils import (disable_proxy, download_item, get_extension_from_url,
-                       get_time_now_as_day, get_time_now_as_hour, unquote_sid)
-from src.validators import ListObjectType
-
+from src.utils import (
+    disable_proxy,
+    download_item,
+    get_extension_from_url,
+    get_time_now_as_day,
+    get_time_now_as_hour,
+    unquote_sid,
+)
+from src.validators import ListObjectType, ListUserType
 
 def parse_args(*args):
     parser = ArgumentParser("InstagramDownloader")
-    parser.add_argument("users", metavar="categories", type=str, nargs="*", help="The categories that has the sessionid and the users to scrape", default=["TEST"])
-    parser.add_argument("--output", "-o", dest="download_path", type=str, help="The folder to store downloaded files in.", default=MEDIA_PATH)
-    parser.add_argument("--download-limit", "-l", dest="download_limit", type=int, help="The number of calls to do per one call. Keep it at a low number to avoid huge payload, but not too low to avoid rate limiting. (Default 3)", default=LIMIT)
+    parser.add_argument(
+        "users",
+        metavar="categories",
+        type=str,
+        nargs="*",
+        help="The categories that has the sessionid and the users to scrape",
+        default=["TEST"],
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        dest="download_path",
+        type=str,
+        help="The folder to store downloaded files in.",
+        default=MEDIA_PATH,
+    )
+    parser.add_argument(
+        "--download-limit",
+        "-l",
+        dest="download_limit",
+        type=int,
+        help="The number of calls to do per one call. Keep it at a low number to avoid huge payload, but not too low to avoid rate limiting. (Default 3)",
+        default=LIMIT,
+    )
 
     input_group = parser.add_argument_group(title="Input Methods")
-    input_group.add_argument("--input-file", "-f", type=str, help="The json file to load session and users from", default=os.path.join("data", "list.json"))
-    input_group.add_argument("--session-id", "-e", type=str, help="Your session id to login as.", default="")
-    input_group.add_argument("--users", "-u", metavar="USERNAME", dest="usernames", type=str, nargs="+", help="The usernames to download", default=[])
+    input_group.add_argument(
+        "--input-file",
+        "-f",
+        type=str,
+        help="The json file to load session and users from",
+        default=os.path.join("data", "list.json"),
+    )
+    input_group.add_argument(
+        "--all-categories", "-a", dest="all_users", action="store_true", help="Automatically goes through all the categories"
+    )
+    input_group.add_argument(
+        "--session-id", "-e", type=str, help="Your session id to login as.", default=""
+    )
+    input_group.add_argument(
+        "--users",
+        "-u",
+        metavar="USERNAME",
+        dest="usernames",
+        type=str,
+        nargs="+",
+        help="The usernames to download",
+        default=[],
+    )
 
     save_group = parser.add_argument_group(title="Download Switches")
-    save_group.add_argument("--disable-save-story", "--no-story", "-s", dest="dl_story", action="store_false", help="Disable downloading Stories if on")
-    save_group.add_argument("--disable-save-posts", "--no-posts", "-p", dest="dl_posts", action="store_false", help="Disable downloading Posts if on")
-    save_group.add_argument("--disable-save-highlights", "--no-highlights", "-i", dest="dl_high", action="store_false", help="Disable downloading Highlights if on")
+    save_group.add_argument(
+        "--disable-save-story",
+        "--no-story",
+        "-s",
+        dest="dl_story",
+        action="store_false",
+        help="Disable downloading Stories if on",
+    )
+    save_group.add_argument(
+        "--disable-save-posts",
+        "--no-posts",
+        "-p",
+        dest="dl_posts",
+        action="store_false",
+        help="Disable downloading Posts if on",
+    )
+    save_group.add_argument(
+        "--disable-save-highlights",
+        "--no-highlights",
+        "-i",
+        dest="dl_high",
+        action="store_false",
+        help="Disable downloading Highlights if on",
+    )
 
     save_only_group = save_group.add_mutually_exclusive_group()
-    save_only_group.add_argument("--save-story-only", "--only-story", "-S", dest="story_only", action="store_true", help="Download stories only")
-    save_only_group.add_argument("--save-posts-only", "--only-posts", "-P", dest="posts_only", action="store_true", help="Download posts only")
-    save_only_group.add_argument("--save-highlights-only", "--only-highlights", "-I", dest="high_only", action="store_true", help="Download Highlights only")
+    save_only_group.add_argument(
+        "--save-story-only",
+        "--only-story",
+        "-S",
+        dest="story_only",
+        action="store_true",
+        help="Download stories only",
+    )
+    save_only_group.add_argument(
+        "--save-posts-only",
+        "--only-posts",
+        "-P",
+        dest="posts_only",
+        action="store_true",
+        help="Download posts only",
+    )
+    save_only_group.add_argument(
+        "--save-highlights-only",
+        "--only-highlights",
+        "-I",
+        dest="high_only",
+        action="store_true",
+        help="Download Highlights only",
+    )
 
-    parser.add_argument("--allow-proxy", "-x", dest="bypass_proxy", action="store_false", help="Allow usage of os proxy. If off (default) then python will bypass proxy.")
+    parser.add_argument(
+        "--allow-proxy",
+        "-x",
+        dest="bypass_proxy",
+        action="store_false",
+        help="Allow usage of os proxy. If off (default) then python will bypass proxy.",
+    )
 
     if args:
         return parser.parse_args(args)
     else:
         return parser.parse_args()
 
+
 if __name__ == "__main__":
 
     args = parse_args()
-    
+
     session_users: List[str] = args.users
     downloads_folder: str = args.download_path
     download_limit: int = args.download_limit
@@ -50,6 +145,7 @@ if __name__ == "__main__":
     input_file: str = args.input_file
     passed_session_id: str = args.session_id
     passed_users: List[str] = args.usernames
+    all_users: bool = args.all_users
 
     dl_story: bool = args.dl_story
     dl_posts: bool = args.dl_posts
@@ -73,27 +169,46 @@ if __name__ == "__main__":
     if bypass_proxy:
         disable_proxy("instagram.com")
 
+    usernames_list: Dict[str, ListUserType]
+
     if passed_session_id or passed_users:
+        if all_users:
+            raise ValueError("When Input File is not specified, All Users flag cannot be used.")
         if not passed_session_id or not passed_users:
-            raise ValueError("When Input File is not specified, both session id and users must be passed.")
-        usernames_list: Dict[str, ListObjectType] = {
+            raise ValueError(
+                "When Input File is not specified, both session id and users must be passed."
+            )
+        session_map = {
+            "S": passed_session_id,
+        }
+        usernames_list = {
             "passed": {
-                "sessionid": passed_session_id,
+                "sessionid": "S",
                 "users": passed_users,
             }
         }
-        args.user = session_users = ["passed"]
+        args.users = session_users = ["passed"]
     else:
         try:
             with open(input_file, "r", encoding="utf-8") as f:
-                usernames_list: Dict[str, ListObjectType] = json.load(f)
+                listobject: ListObjectType = json.load(f)
+                usernames_list = listobject.get("categories")
+                session_map = listobject.get("sessionids")
+                if all_users:
+                    args.users = session_users = list(usernames_list.keys())
         except Exception:
             raise Exception("Failed to load data/list.json")
+
 
     for session_user in session_users:
 
         usernames = usernames_list[session_user].get("users", [])
-        sessionid = usernames_list[session_user].get("sessionid", None)
+
+        sessionid_tag = usernames_list[session_user].get("sessionid", None)
+        if sessionid_tag is None:
+            raise Exception("Invalid Session ID Reference provided")
+
+        sessionid = session_map.get(sessionid_tag, None)
         if sessionid is None:
             raise Exception("Invalid Session ID provided")
         sessionid = unquote_sid(sessionid)
@@ -111,13 +226,15 @@ if __name__ == "__main__":
                 all_usernames = json.load(f)
                 for u_id, u_name in all_usernames.items():
                     if u_name in usernames:
-                        username_mappings[u_id] = u_name        
+                        username_mappings[u_id] = u_name
 
         for username in usernames:
             # Add something to refresh profile pic if pic isn't today
             if username in username_mappings.values():
                 time_str = get_time_now_as_day()
-                pro_pic_path = os.path.join(downloads_folder, username, "profile_pics", time_str)
+                pro_pic_path = os.path.join(
+                    downloads_folder, username, "profile_pics", time_str
+                )
                 if os.path.exists(pro_pic_path + ".jpg"):
                     continue
                 if os.path.exists(pro_pic_path + ".webp"):
@@ -132,31 +249,38 @@ if __name__ == "__main__":
 
             pro_pic_path = os.path.join(downloads_folder, username, "profile_pics")
             os.makedirs(pro_pic_path, exist_ok=True)
-            pro_pic_path = os.path.join(pro_pic_path, get_time_now_as_day() + "." + get_extension_from_url(profile_pic))
+            pro_pic_path = os.path.join(
+                pro_pic_path,
+                get_time_now_as_day() + "." + get_extension_from_url(profile_pic),
+            )
             download_item(profile_pic, pro_pic_path)
-        
+
         with open(usernames_path, "w", encoding="utf-8") as f:
             json.dump(all_usernames, f, indent=4, ensure_ascii=False)
 
         # Traverse stories 3 at a time
         users = list(username_mappings.keys())
         for i in range(0, len(usernames) if dl_story else 0, download_limit):
-            cur_users = users[i:i+download_limit]
+            cur_users = users[i : i + download_limit]
             cur_usernames = [username_mappings[uid] for uid in cur_users]
             print("Getting stories for", " ".join(cur_usernames))
 
             data = instagram.get_reels_data(cur_users)
 
-            for story_data, user_id in instagram.parse_reels_data(data, username_mappings):
+            for story_data, user_id in instagram.parse_reels_data(
+                data, username_mappings
+            ):
                 username = username_mappings[str(user_id)]
                 story_path = os.path.join(downloads_folder, username, "meta")
                 cur_hour = get_time_now_as_hour()
                 story_file = os.path.join(story_path, f"story_{cur_hour}.json")
-                
-                with open(story_file, "w", encoding='utf-8') as f:
+
+                with open(story_file, "w", encoding="utf-8") as f:
                     json.dump(story_data, f, ensure_ascii=False, indent=4)
 
-                instagram.download_list(story_data, username_mappings, "stories", downloads_folder)
+                instagram.download_list(
+                    story_data, username_mappings, "stories", downloads_folder
+                )
 
         for user_id, username in username_mappings.items() if dl_posts else []:
             print("Getting posts for", username, user_id)
@@ -165,7 +289,7 @@ if __name__ == "__main__":
             posts_meta_path = os.path.join(downloads_folder, username, "meta")
             posts_file = os.path.join(posts_meta_path, "posts.json")
             if os.path.exists(posts_file):
-                with open(posts_file, encoding='utf-8') as f:
+                with open(posts_file, encoding="utf-8") as f:
                     old_posts = json.load(f)
             else:
                 old_posts = []
@@ -174,43 +298,61 @@ if __name__ == "__main__":
             full_posts = []
             for posts in instagram.parse_posts_data(posts_data):
                 full_posts.extend(posts)
-            instagram.download_list(full_posts, username_mappings, posts_folder, downloads_folder)
+            instagram.download_list(
+                full_posts, username_mappings, posts_folder, downloads_folder
+            )
 
-
-            with open(posts_file, "w", encoding='utf-8') as f:
+            with open(posts_file, "w", encoding="utf-8") as f:
                 json.dump(full_posts + old_posts, f, ensure_ascii=False, indent=4)
 
         for user_id, username in username_mappings.items() if dl_high else []:
             print("Getting highlights for", username, user_id)
             highlights_data, highlights_ids = instagram.get_highlights_data(user_id)
-            
-            for i in range(0, len(highlights_ids), download_limit): # Walk 3 at a time
-                print(f"Getting page {i//download_limit +1} / {ceil(len(highlights_ids)/download_limit)}")
-                cur_h = highlights_ids[i:i+download_limit]
+
+            for i in range(0, len(highlights_ids), download_limit):  # Walk 3 at a time
+                print(
+                    f"Getting page {i//download_limit +1} / {ceil(len(highlights_ids)/download_limit)}"
+                )
+                cur_h = highlights_ids[i : i + download_limit]
                 data = instagram.get_reels_data(cur_h)
-                
+
                 for j, highlight in enumerate(data["reels"].values()):
                     h_id = highlight["id"].split(":", 1)[-1]
                     items = highlight["items"]
-                    highlights_data[h_id]["reels"].extend(instagram.parse_highlights_data(items))
+                    highlights_data[h_id]["reels"].extend(
+                        instagram.parse_highlights_data(items)
+                    )
 
                     highlights_folder = os.path.join("highlights", h_id)
-                    highlights_folder_full_path = os.path.join(downloads_folder, username, highlights_folder)
+                    highlights_folder_full_path = os.path.join(
+                        downloads_folder, username, highlights_folder
+                    )
                     print(f"Getting highlight {h_id} ({j+1}/{download_limit})")
-                    instagram.download_list(highlights_data[h_id]["reels"], username_mappings, highlights_folder, downloads_folder)
+                    instagram.download_list(
+                        highlights_data[h_id]["reels"],
+                        username_mappings,
+                        highlights_folder,
+                        downloads_folder,
+                    )
 
                     print("Saving name and thumbnail")
                     thumb_url = highlights_data[h_id]["thumbnail_url"]
-                    thumb_path = os.path.join(highlights_folder_full_path, "thumbnail." + get_extension_from_url(thumb_url))
+                    thumb_path = os.path.join(
+                        highlights_folder_full_path,
+                        "thumbnail." + get_extension_from_url(thumb_url),
+                    )
                     download_item(thumb_url, thumb_path)
                     print()
-                    with open(os.path.join(highlights_folder_full_path, "name.txt"), "w", encoding="utf-8") as f:
+                    with open(
+                        os.path.join(highlights_folder_full_path, "name.txt"),
+                        "w",
+                        encoding="utf-8",
+                    ) as f:
                         f.write(highlights_data[h_id]["title"])
-
 
             highlights_path = os.path.join(downloads_folder, username, "meta")
             os.makedirs(highlights_path, exist_ok=True)
 
             highlights_file = os.path.join(highlights_path, "highlights.json")
-            with open(highlights_file, "w", encoding='utf-8') as f:
+            with open(highlights_file, "w", encoding="utf-8") as f:
                 json.dump(highlights_data, f, ensure_ascii=False, indent=4)
--- src/validators.py
+++ src/validators.py
@@ -1,17 +1,24 @@
-from typing import Dict, List, Literal, Optional, TypedDict
+from typing import Dict, List, Literal, Optional, Tuple, TypedDict
 
-
+class ParsedTagUserType(TypedDict):
+    id: str
+    username: str
 class ParsedItemType(TypedDict):
     id: str
     owner: str
+    owner_username: str
+    tagged_users: List[ParsedTagUserType]
     parent: Optional[str]
     image_url: str
     video_url: Optional[str]
     besties_only: bool
+    time: int
 
 class UserType(TypedDict):
     pk: str
     id: str
+    username: str
+    full_name: str
     profile_pic_url_hd: Optional[str]
     profile_pic_url: str
 
@@ -19,6 +26,12 @@ class MediaCandidateType(TypedDict):
     width: int
     height: int
     url: str
+
+class UserMediaTagType(TypedDict):
+    user: UserType
+    position: Tuple[float, float]
+    start_time_in_video_in_sec: Optional[float]
+    duration_in_video_in_sec: Optional[float]
 
 class ReelItemType(TypedDict):
     pk: str
@@ -28,7 +41,13 @@ class ReelItemType(TypedDict):
     audience: str
     image_versions2: Dict[Literal["candidates"], List[MediaCandidateType]]
     video_versions: List[MediaCandidateType]
+    taken_at: int # TimeStamp
+    usertags: Dict[str, List[UserMediaTagType]]
 
-class ListObjectType(TypedDict):
+class ListUserType(TypedDict):
     sessionid: str
     users: List[str]
+
+class ListObjectType(TypedDict):
+    categories: Dict[str, ListUserType]
+    sessionids: Dict[str, str]
